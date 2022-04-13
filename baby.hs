0528773bfe1ee4c6f5318f53da411fcb00084ed6
jiaxen <jiaxen.lau@gmail.com>
2022-04-13 22:26:42 +0100

    learning list comprehensions

--- baby.hs
+++ baby.hs
@@ -3,3 +3,5 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
     then x
     else x * 2  
+-- A List comprehension to produce a list of tuples of right angled triangles with longest side <= 10
+rightTriangle = [(a,b,c) | a <- [1..10], b <- [1..a], c <- [1..b], c^2 + b^2 == a^2]
